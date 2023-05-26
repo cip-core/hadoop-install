0f726164829534dd800f034ef1e557532ab4bcc7
satyamkumar <satyam.kumar@socgen.com>
2023-05-26 19:30:51 +0000

    fixed datanode error

--- create_dirs.sh
+++ create_dirs.sh
@@ -13,13 +13,13 @@ DATA_DIR=/opt/hadoop/data/dn
 HDFS_LOG_DIR=/opt/hadoop/logs
 
 echo "Create namenode dir"
-sudo mkdir -p $NAME_DIR
-sudo chmod -R 755 $NAME_DIR
+mkdir -p $NAME_DIR
+chmod -R 777 $NAME_DIR
 
 echo "Create datanode dir"
-sudo mkdir -p $DATA_DIR
-sudo chmod -R 755 $DATA_DIR
+mkdir -p $DATA_DIR
+chmod -R 777 $DATA_DIR
 
 echo "Create log dir"
-sudo mkdir -p $HDFS_LOG_DIR
-sudo chmod -R 755 $HDFS_LOG_DIR
+mkdir -p $HDFS_LOG_DIR
+chmod -R 777 $HDFS_LOG_DIR
